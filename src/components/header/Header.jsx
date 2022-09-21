5235d38eba69ebf35c008da9a5a5fe03a9f523d3
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-09-21 14:43:37 +0200

    fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,21 +6,22 @@ import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <header id='home'>
+    <section id='home'>
       <div className="container header__container">
 
         <h1 className='h__1'>Hello there!</h1>
         <h3 className="text-light"> I am Sergio, a Fullstack Developer from Chile based in Berlin.</h3>
-        <CTA />
+
         <HeaderSocials />
 
         <div className="me">
           <img src={profile} alt="me" className='my__picture' />
         </div>
+        <CTA />
 
 
       </div>
-    </header>
+    </section>
   )
 }
 
